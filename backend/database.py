f46a09cff0ac529381354b9eb660bec50f652316
Yutong13 <yutong13@ualberta.ca>
2024-05-04 01:11:46 +0800

    create Database class using singleton pattern

--- backend/database.py
+++ backend/database.py
@@ -1,2 +1,17 @@
-import pymongo
+from pymongo import MongoClient
+class Database:
+    _instance = None
 
+    def __new__(cls, host: str = "localhost", port: int = 27017):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+            cls.__mongo_client = MongoClient(host, port)
+            cls.db_name = "resturant"
+            cls.database = cls.__mongo_client[cls.db_name]
+        return cls._instance
+    
+    def __del__(self):
+        return self.__mongo_client.close()
+    
+    def close(self):
+        return self.__del__();
--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,39 @@
 from fastapi import FastAPI, Cookie, Form, HTTPException, Request, Response, status
-app = FastAPI()
+from database import Database
+from os import getenv
+import uvicorn
+from contextlib import asynccontextmanager
+import asyncio
 
+@asynccontextmanager
+async def lifespan(app:FastAPI):
+    startup_db_client()
+    yield
+    Database.close()
+
+app = FastAPI(lifespan=lifespan)
+
+
+def startup_db_client():
+    args = {}
+    
+    # grab address and port from system variables
+    host = getenv('MONGODB_ADDR')
+    port = getenv('MONGODB_PORT')
+
+    # Check if using default path
+    if host == None:
+        host = 'localhost'
+    if port == None:
+        port = 27017
+
+    if host:
+        args['host'] = host
+    if port:
+        args['port'] = port
+    print(args)
+    app.database = Database(host,port).database
+    print("Connected to MongoDB")
 
 @app.get("/")
 async def root():
@@ -9,3 +42,6 @@ async def root():
 @app.post("/login")
 async def login(request:Request, response: Response, username: str, password:str):
     return True
+
+if __name__ == "__main__":
+    uvicorn.run(app=app, port=8000)
