c0bbda4aabd4a256a8328032b880d0261283bd43
hengz <hzyharry@connect.hku.hk>
2024-05-30 18:31:38 +0800

    Merge Menu to Backend

--- backend/main.py
+++ backend/main.py
@@ -10,10 +10,16 @@ from fastapi import (
 )
 from fastapi.middleware.cors import CORSMiddleware
 from auth.router import router as auth_router
+from fastapi import FastAPI, Request, Response, HTTPException
 from database import Database
 from os import getenv
 import uvicorn
 from contextlib import asynccontextmanager
+
+
+from menu.routes import router as menu_router
+from models import Menu, MenuUpdate
+from constants import menuDb
 
 
 @asynccontextmanager
@@ -24,6 +30,8 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(menu_router, tags=["menus"], prefix="/menu")
 
 
 def startup_db_client():
@@ -61,6 +69,43 @@ app.add_middleware(
 
 
 app.include_router(auth_router)
+
+
+
+@app.get("/menus")
+async def get_menus():
+    return menuDb
+
+
+@app.post("/menus")
+async def create_menu(menu: Menu):
+    menuDb.append(menu)
+    return {"id": menu.id}
+
+
+@app.delete("/menus/{_id}")
+async def delete_menu(_id):
+    for menu in menuDb:
+        if menu.id == _id:
+            menuDb.remove(menu)
+            return
+    raise HTTPException(
+        status_code=404, detail=f"Delete user failed, id {_id} not found."
+    )
+
+
+@app.put("/menus/{_id}")
+async def update_user(menu_update: MenuUpdate, _id):
+    for menu in menuDb:
+        if menu.id == _id:
+            if menu_update.name is not None:
+                menu.name = menu_update.name
+            if menu_update.price is not None:
+                menu.price = menu_update.price
+            if menu_update.weight is not None:
+                menu.weight = menu_update.weight
+        return menu.id
+    raise HTTPException(status_code=404, detail=f"Could not find user with id: {_id}")
 
 
 if __name__ == "__main__":
