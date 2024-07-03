a537ad451d32b888c9a9314bda5903dcf4f247b6
Harrison Heng <39851618+hzyharry@users.noreply.github.com>
2024-07-03 21:26:22 +0800

    Update models.py

--- backend/menu/models.py
+++ backend/menu/models.py
@@ -7,17 +7,17 @@ class Ingredient(BaseModel):
     weight: float
 
 class Menu(BaseModel):
-    __pydantic_config__ = ConfigDict(extra='forbid')
+#    __pydantic_config__ = ConfigDict(extra='forbid')
 
     id: int = Field(...)
     name: str = Field(...)
     price: float = Field(...)
     weight: float = Field(...)
-    ingredient: List[Ingredient] = Field(...)
+    ingredient: List = Field(...)
     sold: int = Field(...)
     availability: bool = Field(...)
     desc: str = Field(...)
-    image: bytes = Field(...)
+    image: str = Field(...)
 
     class Config:
         populate_by_name = True
@@ -27,11 +27,11 @@ class Menu(BaseModel):
                 "name": "Beef Chow Fun",
                 "price": "45.50",
                 "weight": "450",
-                "ingredient": [{"id": 1, "name": "Carrot", "weight": 120.0}], # [{carrot: 120.0}. {}, {}]
+                "ingredient": [{"id": 1, "name": "Carrot", "weight": 120.0}],
                 "sold": "45",
                 "availability": "true",
                 "desc": "Dry fried beef Shahe noodles",
-                "image": b"..."
+                "image": "123"
             }
         }
 
@@ -43,7 +43,7 @@ class MenuUpdate(BaseModel):
     sold: Optional[int]
     availability: Optional[bool]
     desc: Optional[str]
-    image: Optional[bytes]
+    image: Optional[str]
 
     class Config:
         json_schema_extra = {
@@ -55,6 +55,6 @@ class MenuUpdate(BaseModel):
                 "sold": "45",
                 "availability": "true",
                 "desc": "Dry fried beef Shahe noodles",
-                "image": b"..."
+                "image": "123"
             }
         }
